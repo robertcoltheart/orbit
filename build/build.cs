ff3a12e462fc43eb7859e42e753a7ef645eef740
Robert Coltheart <robert.coltheart@gmail.com>
2021-02-17 21:37:16 +0800

    Updates

--- build/build.cs
+++ build/build.cs
@@ -4,68 +4,62 @@ using System.Text.Json;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
-public class build
+var version = GetGitVersion();
+
+Target("clean", () =>
 {
-    public static void Main(string[] args)
+    Run("dotnet", "clean --configuration Release");
+
+    if (Directory.Exists("artifacts"))
     {
-        var version = GetGitVersion();
-
-        Target("clean", () =>
-        {
-            Run("dotnet", "clean");
-
-            if (Directory.Exists("artifacts"))
-            {
-                Directory.Delete("artifacts", true);
-            }
-        });
-
-        Target("restore", DependsOn("clean"), () =>
-        {
-            Run("dotnet", "restore");
-        });
-
-        Target("build", DependsOn("restore"), () =>
-        {
-            Run("dotnet", "build " +
-                          "--no-restore " +
-                          "--configuration Release " +
-                          $"/p:Version={version.SemVer} " +
-                          $"/p:AssemblyVersion={version.AssemblySemVer} " +
-                          $"/p:FileVersion={version.AssemblySemFileVer} " +
-                          $"/p:InformationalVersion={version.InformationalVersion}");
-        });
-
-        Target("test", DependsOn("build"), () =>
-        {
-            Run("dotnet", "test --configuration Release --no-restore --no-build");
-        });
-
-        Target("package", DependsOn("build", "test"), () =>
-        {
-            Run("dotnet", $"pack --configuration Release --no-restore --no-build --output artifacts /p:Version={version.SemVer}");
-        });
-
-        Target("publish", DependsOn("package"), () =>
-        {
-            var apiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY");
-
-            Run("dotnet", $"nuget push {Path.Combine("artifacts", "*.nupkg")} --api-key {apiKey} --source https://api.nuget.org/v3/index.json");
-        });
-
-        Target("default", DependsOn("package"));
-
-        RunTargetsAndExit(args);
+        Directory.Delete("artifacts", true);
     }
+});
 
-    private static GitVersion GetGitVersion()
-    {
-        Run("dotnet", "tool restore");
+Target("restore", DependsOn("clean"), () =>
+{
+    Run("dotnet", "restore");
+});
 
-        var value = Read("dotnet", "dotnet-gitversion");
+Target("build", DependsOn("restore"), () =>
+{
+    Run("dotnet", "build " +
+                  "--no-restore " +
+                  "--configuration Release " +
+                  $"/p:Version={version.SemVer} " +
+                  $"/p:AssemblyVersion={version.AssemblySemVer} " +
+                  $"/p:FileVersion={version.AssemblySemFileVer} " +
+                  $"/p:InformationalVersion={version.InformationalVersion}");
+});
 
-        return JsonSerializer.Deserialize<GitVersion>(value);
-    }
+Target("test", DependsOn("build"), () =>
+{
+    Run("dotnet", "test --configuration Release --no-restore --no-build");
+});
+
+Target("package", DependsOn("build", "test"), () =>
+{
+    Run("dotnet", $"pack --configuration Release --no-restore --no-build --output artifacts /p:Version={version.SemVer}");
+});
+
+Target("publish", DependsOn("package"), () =>
+{
+    var apiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY");
+
+    Run("dotnet", $"nuget push {Path.Combine("artifacts", "*.nupkg")} --api-key {apiKey} --source https://api.nuget.org/v3/index.json");
+});
+
+Target("default", DependsOn("package"));
+
+RunTargetsAndExit(args);
+
+GitVersion GetGitVersion()
+{
+    Run("dotnet", "tool restore");
+
+    var value = Read("dotnet", "dotnet-gitversion");
+
+    return JsonSerializer.Deserialize<GitVersion>(value);
 }
 
 public class GitVersion
